3bbc3b8533c261946b8a15f338fe9c7d642a3362
AlexW2311 <alex.williams2311@icloud.com>
2025-03-09 17:31:34 -0700

    Window manipulation and project config

--- src/main.cpp
+++ src/main.cpp
@@ -2,16 +2,21 @@
 
 int main()
 {
-    auto window = sf::RenderWindow(sf::VideoMode({1920u, 1080u}), "CMake SFML Project");
-    window.setFramerateLimit(144);
+    auto window = sf::RenderWindow(sf::VideoMode({800u, 600u}), "CMake SFML Project");
+    window.setFramerateLimit(60);
 
     while (window.isOpen())
     {
-        while (const std::optional event = window.pollEvent())
+        while (const std::optional event = window.pollEvent()) //Event loop
         {
             if (event->is<sf::Event::Closed>())
             {
                 window.close();
+            }
+            else if (const auto* keyPressed = event->getIf<sf::Event::KeyPressed>())
+            {
+                if (keyPressed->scancode == sf::Keyboard::Scancode::Escape)
+                    window.close();
             }
         }
 
